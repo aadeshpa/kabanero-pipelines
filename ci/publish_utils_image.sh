01ba9efbf18c118d8f9569e20b1755dc5113322e
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-06-01 15:59:03 -0400

    Update publish_utils_image.sh

--- ci/publish_utils_image.sh
+++ ci/publish_utils_image.sh
@@ -10,6 +10,14 @@ if [ ! -z "$TRAVIS_TAG" ]; then
    cd ./pipelines/docker/kabanero-utils/
    pwd
    ls -la
+   
+   echo "Running Docker build"  
+   docker build -t $IMAGE_NAME:$IMAGE_TAG_NAME .
+   echo "docker build completed successfully."
+   echo "Printing the docker images"
+
+   echo "printing all images";
+   docker images --digests;
 else
        echo "It is not a tagged commit or, the TRAVIS_TAG=$TRAVIS_TAG is empty"
 fi
