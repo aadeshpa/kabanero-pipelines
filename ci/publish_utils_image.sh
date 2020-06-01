5955d80671ffac550c8a2cb9f44dfdf92ee8eb57
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-06-01 16:48:53 -0400

    Update publish_utils_image.sh

--- ci/publish_utils_image.sh
+++ ci/publish_utils_image.sh
@@ -13,22 +13,27 @@ if [ ! -z "$TRAVIS_TAG" ]; then
    
    echo "Running Docker build"  
    docker build -t $IMAGE_NAME:$TRAVIS_TAG .
-   echo "docker build completed successfully."
-   echo "Printing the docker images"
+   if [ $? != 0 ]; then
+      echo "Docker Image $IMAGE_NAME:$TRAVIS_TAG was build successfully"
+     
+      echo "printing all images"
+      docker images --digests
+   
+      echo "Pushing the image$IMAGE_NAME:$IMAGE_TAG_NAME) to docker.io/$(USER_NAME)/$IMAGE_NAME:$TRAVIS_TAG "
+      echo "DOCKER_USERNAME=$DOCKER_USERNAME"
+      echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
+      docker push $IMAGE_NAME:$TRAVIS_TAG
+      docker images --digests
+   
+      image_digest_value_withquote=$(docker inspect --format='{{json .RepoDigests}}' $IMAGE_NAME:$TRAVIS_TAG | jq 'values[0]')
+      echo "image_digest_value_withquote=$image_digest_value_withquote"
+      image_digest_value=$(sed -e 's/^"//' -e 's/"$//' <<<"$image_digest_value_withquote")
+      echo "image_digest_value=$image_digest_value"
+   else
+      echo "[ERROR] The container image $IMAGE_NAME:$TRAVIS_TAG build failed, please check the logs."
+      exit 1
+   fi
 
-   echo "printing all images"
-   docker images --digests
-   
-   echo "Pushing the image$IMAGE_NAME:$IMAGE_TAG_NAME) to docker.io/$(USER_NAME)/$IMAGE_NAME:$TRAVIS_TAG "
-   echo "DOCKER_USERNAME=$DOCKER_USERNAME"
-   echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
-   docker push $IMAGE_NAME:$TRAVIS_TAG
-   docker images --digests
-   
-   image_digest_value_withquote=$(docker inspect --format='{{json .RepoDigests}}' $IMAGE_NAME:$TRAVIS_TAG | jq 'values[0]')
-   echo "image_digest_value_withquote=$image_digest_value_withquote"
-   image_digest_value=$(sed -e 's/^"//' -e 's/"$//' <<<"$image_digest_value_withquote")
-   echo "image_digest_value=$image_digest_value"
 else
        echo "It is not a tagged commit or, the TRAVIS_TAG=$TRAVIS_TAG is empty"
 fi
