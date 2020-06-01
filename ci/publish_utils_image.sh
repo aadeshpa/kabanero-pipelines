b724778576ee7793b087474bd4195a6e9a911854
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-06-01 16:21:27 -0400

    Update publish_utils_image.sh

--- ci/publish_utils_image.sh
+++ ci/publish_utils_image.sh
@@ -16,14 +16,20 @@ if [ ! -z "$TRAVIS_TAG" ]; then
    echo "docker build completed successfully."
    echo "Printing the docker images"
 
-   echo "printing all images";
-   docker images --digests;
+   echo "printing all images"
+   docker images --digests
    
-   echo "Pushing the image$IMAGE_NAME:$IMAGE_TAG_NAME) to docker.io/$(USER_NAME)/$IMAGE_NAME:$IMAGE_TAG_NAME ";
-   echo "DOCKER_USERNAME=$DOCKER_USERNAME";
-   echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin;
-   docker push $IMAGE_NAME:$IMAGE_TAG_NAME;
-   docker images --digests;
+   echo "Pushing the image$IMAGE_NAME:$IMAGE_TAG_NAME) to docker.io/$(USER_NAME)/$IMAGE_NAME:$IMAGE_TAG_NAME "
+   echo "DOCKER_USERNAME=$DOCKER_USERNAME"
+   echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
+   docker push $IMAGE_NAME:$IMAGE_TAG_NAME
+   docker images --digests
+   
+   image_digest_value_withquote=$(docker inspect --format='{{json .RepoDigests}}' $IMAGE_NAME:$IMAGE_TAG_NAME | jq 'values[0]');
+   echo "image_digest_value_withquote=$image_digest_value_withquote";
+   image_digest_value=$(sed -e 's/^"//' -e 's/"$//' <<<"$image_digest_value_withquote");
+   echo "image_digest_value=$image_digest_value";
+     
 else
        echo "It is not a tagged commit or, the TRAVIS_TAG=$TRAVIS_TAG is empty"
 fi
