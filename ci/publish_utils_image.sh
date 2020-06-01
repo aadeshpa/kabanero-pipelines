15b9677ca7efbcc85c80d5fbafa144780d1d7521
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-06-01 15:54:09 -0400

    Update publish_utils_image.sh

--- ci/publish_utils_image.sh
+++ ci/publish_utils_image.sh
@@ -2,14 +2,14 @@
 set -e
 
 echo "Running publish_utils_image.sh script"
-echo "ls -la ../pipelines/docker/kabanero-utils/"
-cd ../pipelines/docker/kabanero-utils/
-pwd
-ls -la
+
 
 echo "TRAVIS_TAG=$TRAVIS_TAG"
 if [ ! -z "$TRAVIS_TAG" ]; then
    echo "TRAVIS_TAG=$TRAVIS_TAG found and not empty."
+   cd ./pipelines/docker/kabanero-utils/
+   pwd
+   ls -la
 else
        echo "It is not a tagged commit or, the TRAVIS_TAG=$TRAVIS_TAG is empty"
 fi
