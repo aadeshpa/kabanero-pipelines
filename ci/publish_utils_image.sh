1cc6ecd19f515836405178cd21a4ba3af1061d0f
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-06-02 11:41:49 -0400

    Update publish_utils_image.sh

--- ci/publish_utils_image.sh
+++ ci/publish_utils_image.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 set -e
 
-echo "Running publish_utils_image.sh script"
+#echo "Running publish_utils_image.sh script"
 
 
 echo "TRAVIS_TAG=$TRAVIS_TAG"
 if [ ! -z "$TRAVIS_TAG" ] && [ ! -z "$DOCKER_USERNAME" ] && [ ! -z "$DOCKER_PASSWORD" ]; then
-   echo "TRAVIS_TAG=$TRAVIS_TAG found and not empty."
+   #echo "TRAVIS_TAG=$TRAVIS_TAG found and not empty."
    cd ./pipelines/docker/kabanero-utils/
-   pwd
-   ls -la
+   #pwd
+   #ls -la
    
-   echo "Running Docker build"  
+   #echo "Running Docker build"  
    docker build -t $IMAGE_NAME:$TRAVIS_TAG .
    if [ $? == 0 ]; then
       echo "Docker image $IMAGE_NAME:$TRAVIS_TAG was build successfully"
      
-      echo "printing all images"
-      docker images --digests
+      #echo "printing all images"
+      #docker images --digests
    
-      echo "Pushing the image $IMAGE_NAME:$IMAGE_TAG_NAME to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG "
-      echo "DOCKER_USERNAME=$DOCKER_USERNAME"
+      #echo "Pushing the image $IMAGE_NAME:$IMAGE_TAG_NAME to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG "
+      #echo "DOCKER_USERNAME=$DOCKER_USERNAME"
       echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
       docker push $IMAGE_NAME:$TRAVIS_TAG
       if [ $? == 0 ]; then
-         echo "The docker image $IMAGE_NAME:$TRAVIS_TAG was successfully pushed to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG"
+        # echo "The docker image $IMAGE_NAME:$TRAVIS_TAG was successfully pushed to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG"
           docker images --digests
    
           image_digest_value_withquote=$(docker inspect --format='{{json .RepoDigests}}' $IMAGE_NAME:$TRAVIS_TAG | jq 'values[0]')
