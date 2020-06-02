bf801134983883d41502bd8acd7098d1ac596233
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-06-02 14:55:29 -0400

    Update publish_utils_image.sh

--- ci/publish_utils_image.sh
+++ ci/publish_utils_image.sh
@@ -5,7 +5,7 @@ set -e
 
 
 echo "TRAVIS_TAG=$TRAVIS_TAG"
-echo "Env variable from within the script publishImageStatus=${publishImageStatus}"
+#echo "Env variable from within the script publishImageStatus=${publishImageStatus}"
 if [ ! -z "$TRAVIS_TAG" ] && [ ! -z "$DOCKER_USERNAME" ] && [ ! -z "$DOCKER_PASSWORD" ]; then
    #echo "TRAVIS_TAG=$TRAVIS_TAG found and not empty."
    cd ./pipelines/docker/kabanero-utils/
@@ -20,20 +20,18 @@ if [ ! -z "$TRAVIS_TAG" ] && [ ! -z "$DOCKER_USERNAME" ] && [ ! -z "$DOCKER_PASS
       #echo "printing all images"
       #docker images --digests
    
-      #echo "Pushing the image $IMAGE_NAME:$IMAGE_TAG_NAME to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG "
+      echo "[INFO] Pushing the image $IMAGE_NAME:$IMAGE_TAG_NAME to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG "
       #echo "DOCKER_USERNAME=$DOCKER_USERNAME"
       echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
       docker push $IMAGE_NAME:$TRAVIS_TAG
       if [ $? == 0 ]; then
-        # echo "The docker image $IMAGE_NAME:$TRAVIS_TAG was successfully pushed to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG"
-          docker images --digests
+          #echo "[INFO] The docker image $IMAGE_NAME:$TRAVIS_TAG was successfully pushed to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG"
+          #docker images --digests
    
           image_digest_value_withquote=$(docker inspect --format='{{json .RepoDigests}}' $IMAGE_NAME:$TRAVIS_TAG | jq 'values[0]')
           #echo "image_digest_value_withquote=$image_digest_value_withquote"
           image_digest_value=$(sed -e 's/^"//' -e 's/"$//' <<<"$image_digest_value_withquote")
-          #echo "image_digest_value=$image_digest_value"
-          publishImageStatus=true
-          echo "publishImageStatus = ${publishImageStatus} after the digest value found."
+          echo "image_digest_value=$image_digest_value"
           echo "$image_digest_value"
           
       else
