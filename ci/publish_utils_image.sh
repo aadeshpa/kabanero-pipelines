d8608f0bc4f486b78c924875886203bbac5b82e7
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-06-03 09:32:03 -0400

    Update publish_utils_image.sh

--- ci/publish_utils_image.sh
+++ ci/publish_utils_image.sh
@@ -28,11 +28,11 @@ if [ ! -z "$TRAVIS_TAG" ] && [ ! -z "$DOCKER_USERNAME" ] && [ ! -z "$DOCKER_PASS
           #echo "[INFO] The docker image $IMAGE_NAME:$TRAVIS_TAG was successfully pushed to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG"
           #docker images --digests
    
-          image_digest_value_withquote=$(docker inspect --format='{{json .RepoDigests}}' $IMAGE_NAME:$TRAVIS_TAG | jq 'values[0]')
+          #image_digest_value_withquote=$(docker inspect --format='{{json .RepoDigests}}' $IMAGE_NAME:$TRAVIS_TAG | jq 'values[0]')
           #echo "image_digest_value_withquote=$image_digest_value_withquote"
-          image_digest_value=$(sed -e 's/^"//' -e 's/"$//' <<<"$image_digest_value_withquote")
-          echo "image_digest_value=$image_digest_value"
-          echo "$image_digest_value"
+          #image_digest_value=$(sed -e 's/^"//' -e 's/"$//' <<<"$image_digest_value_withquote")
+          #echo "image_digest_value=$image_digest_value"
+          #echo "$image_digest_value"
           
       else
         echo "[ERROR] The docker push failed for this image docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG, please check the logs"
