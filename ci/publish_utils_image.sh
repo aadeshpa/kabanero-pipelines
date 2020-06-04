cb3374f93b63890a668ea8404156aaf1eb350d1d
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-06-04 15:03:44 -0400

    Update publish_utils_image.sh

--- ci/publish_utils_image.sh
+++ ci/publish_utils_image.sh
@@ -13,26 +13,22 @@ if [ ! -z "$TRAVIS_TAG" ] && [ ! -z "$DOCKER_USERNAME" ] && [ ! -z "$DOCKER_PASS
    cd ./pipelines/docker/kabanero-utils/
    
    #echo "Running Docker build"  
-   docker build -t $IMAGE_NAME:$TRAVIS_TAG .
+   docker build -t $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG .
    if [ $? == 0 ]; then
-      echo "[INFO] Docker image $IMAGE_NAME:$TRAVIS_TAG was build successfully"
-     
-      #echo "printing all images"
-      #docker images --digests
-   
-      echo "[INFO] Pushing the image $IMAGE_NAME:$IMAGE_TAG_NAME to docker.io/$IMAGE_NAME:$TRAVIS_TAG "
+      echo "[INFO] Docker image $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG was build successfully" 
+      echo "[INFO] Pushing the image $DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG_NAME to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG "
       echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
-      docker push $IMAGE_NAME:$TRAVIS_TAG
+      docker push $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG
       if [ $? == 0 ]; then
-          echo "[INFO] The docker image $IMAGE_NAME:$TRAVIS_TAG was successfully pushed to docker.io/$IMAGE_NAME:$TRAVIS_TAG"     
+          echo "[INFO] The docker image $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG was successfully pushed to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG"     
       else
-        echo "[ERROR] The docker push failed for this image docker.io/$IMAGE_NAME:$TRAVIS_TAG, please check the logs"
+        echo "[ERROR] The docker push failed for this image docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG, please check the logs"
         exit 1
       fi    
    else
-      echo "[ERROR] The docker image $IMAGE_NAME:$TRAVIS_TAG build failed, please check the logs."
+      echo "[ERROR] The docker image $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG build failed, please check the logs."
       exit 1
    fi
 else
-       echo "[INFO] This travis build is not for a tagged TRAVIS_TAG and its empty, hence skipping the build and publish of the image $IMAGE_NAME"
+       echo "[INFO] This travis build is not for a tagged TRAVIS_TAG and its empty, hence skipping the build and publish of the image $DOCKER_USERNAME/$IMAGE_NAME"
 fi
