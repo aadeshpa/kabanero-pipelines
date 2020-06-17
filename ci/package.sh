f8ac541dcce973935dfdb00ea4c6ecb09aa12084
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-06-17 17:03:53 -0400

    Update package.sh

--- ci/package.sh
+++ ci/package.sh
@@ -74,6 +74,8 @@ elif [[ ( -z "$TRAVIS_TAG" ) && ( -z "$DOCKER_USERNAME" ) && ( -z "$DOCKER_PASSW
  echo "sourcing config file for fetching image tag name and digest value"
  . image_digest_mapping.config
  echo "sourcing done."
+ echo "release_tag from file=$release_tag"
+ echo "kabanero_utils_image_digest=$kabanero_utisl_image_digest"
 else
  echo "[Warning] The kabaneo-utils image was not build and pushed to dockerhub for this build, because one or more of the env variables TRAVIS_TAG=$TRAVIS_TAG or DOCKER_USERNAME=DOCKER_USERNAME or DOCKER_PASSWORD are empty "
 fi
