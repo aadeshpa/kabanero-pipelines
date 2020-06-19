af307aa91735799b2ab839ca981a3474c238a5fe
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-06-19 11:00:15 -0400

    Update publish_utils_image.sh

--- ci/publish_utils_image.sh
+++ ci/publish_utils_image.sh
@@ -9,25 +9,29 @@ set -e
 
 echo "[INFO] Executing the script /ci/publish_utils_image.sh for TRAVIS_TAG=$TRAVIS_TAG"
 #echo "Env variable from within the script publishImageStatus=${publishImageStatus}"
-if [ ! -z "$TRAVIS_TAG" ] && [ ! -z "$DOCKER_USERNAME" ] && [ ! -z "$DOCKER_PASSWORD" ]; then
-   cd ./pipelines/docker/kabanero-utils/
+if [ ! -z "$TRAVIS_TAG" ] ; then
+   if [[ (! -z "$DOCKER_USERNAME") &&  (! -z "$DOCKER_PASSWORD") ]]; then
+     cd ./pipelines/docker/kabanero-utils/
    
-   #echo "Running Docker build"  
-   docker build -t $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG .
-   if [ $? == 0 ]; then
-      echo "[INFO] Docker image $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG was build successfully" 
-      echo "[INFO] Pushing the image $DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG_NAME to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG "
-      echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
-      docker push $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG
-      if [ $? == 0 ]; then
-          echo "[INFO] The docker image $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG was successfully pushed to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG"     
-      else
-        echo "[ERROR] The docker push failed for this image docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG, please check the logs"
+     #echo "Running Docker build"  
+     docker build -t $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG .
+     if [ $? == 0 ]; then
+        echo "[INFO] Docker image $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG was build successfully" 
+        echo "[INFO] Pushing the image $DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG_NAME to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG "
+        echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
+        docker push $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG
+        if [ $? == 0 ]; then
+            echo "[INFO] The docker image $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG was successfully pushed to docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG"     
+        else
+          echo "[ERROR] The docker push failed for this image docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG, please check the logs"
+          exit 1
+        fi    
+     else
+        echo "[ERROR] The docker image $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG build failed, please check the logs."
         exit 1
-      fi    
+     fi
    else
-      echo "[ERROR] The docker image $DOCKER_USERNAME/$IMAGE_NAME:$TRAVIS_TAG build failed, please check the logs."
-      exit 1
+     echo "[INFO] The travis build is for TRAVIS_TAG=$TRAVIS_TAG but DOCKER_USERNAME and/or DOCKER_PASSWORD are not provided, skipping the build and publish of the utils image." 
    fi
 else
        echo "[INFO] This travis build is not for a tagged TRAVIS_TAG and its empty, hence skipping the build and publish of the image $DOCKER_USERNAME/$IMAGE_NAME"
