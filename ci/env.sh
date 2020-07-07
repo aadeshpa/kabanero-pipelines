e39552034ddb52d5d1ae414759799513e37eda1f
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-07-07 12:33:48 -0400

    Update env.sh

--- ci/env.sh
+++ ci/env.sh
@@ -31,8 +31,8 @@ fi
 # export IMAGE_REGISTRY
 # export IMAGE_REGISTRY_USERNAME
 # export IMAGE_REGISTRY_PASSWORD
-# export UTILS_IMAGE_NAME
-# export UTILS_IMAGE_TAG
+export UTILS_IMAGE_NAME=kabanero-utils
+export UTILS_IMAGE_TAG
 
 # Organization for images
 # export IMAGE_REGISTRY_ORG=kabanero
