f5ddb1a281c6689e2319b8f9e8dccd7745337f00
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2020-07-16 11:33:48 -0400

    Update package.sh

--- ci/package.sh
+++ ci/package.sh
@@ -173,6 +173,10 @@ if [[ (! -z "$IMAGE_REGISTRY") && (! -z "$IMAGE_REGISTRY_USERNAME" ) && ( ! -z "
 
    elif [[ ( ! -z "$image_build_option" ) && ( "$image_build_option" == "buildah" ) ]]; then
       echo "[INFO] Fetching the image digest value for image $IMAGE_REGISTRY/$IMAGE_REGISTRY_USERNAME/$UTILS_IMAGE_NAME:$UTILS_IMAGE_TAG using skopeo inspect"
+      
+      image_digest_value=$( skopeo inspect docker://"$IMAGE_REGISTRY_HOST"/"$PROJECT"/"$STACK_NAME":"$VERSION" | jq '.Digest' )
+      echo "[INFO] using skopeo image_digest_value=$image_digest_value"
+      
    fi
    
    echo "[INFO] Replacing the utils container image string from 'image : $image_original_string' with 'image : $image_digest_value' in all the pipeline task yaml files";
